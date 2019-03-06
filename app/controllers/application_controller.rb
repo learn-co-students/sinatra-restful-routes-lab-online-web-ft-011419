fdd688466839c6841e49565521dfd554780fa737
gbs4ever <gbs4ever2@gmail.com>
2019-03-06 16:15:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+get '/' do 
+erb :index
+end
 
-  # code actions here!
+get '/recipes/new' do
+erb :new
+end
+
+post  '/recipes' do
+
+end
+
+
 
 end
